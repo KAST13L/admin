c9399f2b1b50101b531418ac5bf26e19ceebe227
KAST13L <o4enbsmeshno@gmail.com>
2022-12-26 23:42:47 +0200

    feat: add menu-list to admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import buildHasuraProvider from 'ra-data-hasura';
+import {Admin, DataProvider, Resource} from 'react-admin';
+import {CssBaseline} from "@mui/material";
+import {MenuList} from "./modules/menu/components/menu-list/menu-list.component";
 
 export const App = () => {
+
+    const [dataProvider, setDataProvider] = useState<DataProvider<string> | null>(null);
+
+    useEffect(() => {
+        const buildDataProvider = async () => {
+            const dataProvider = await buildHasuraProvider({
+                clientOptions: {uri: 'http://localhost:8080/v1/graphql'},
+            });
+            setDataProvider(dataProvider);
+        };
+        buildDataProvider();
+    }, []);
+
+    if (!dataProvider) return <p>Loading...</p>;
+
     return (
-        <div>
-            app
-        </div>
+        <>
+            <CssBaseline/>
+            <Admin dataProvider={dataProvider}>
+                <Resource name="menu" list={MenuList}/>
+            </Admin>
+        </>
     );
 };
-
